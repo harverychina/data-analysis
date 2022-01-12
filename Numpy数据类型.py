7c4149f29ef46c2eee9d04cec69d134d7918427e
harverychina <harverychina@126.com>
2022-01-12 15:06:05 +0800

    更新一下Numpy数据类型内容

--- Numpy数据类型.py
+++ Numpy数据类型.py
@@ -4,3 +4,22 @@ import numpy as np
 
 dt = np.dtype(np.int32)
 print(dt)
+
+dt = np.dtype('i8')
+print(dt)
+
+dt = np.dtype('<i4')
+print(dt)
+
+dt = np.dtype([('age', np.int8)])
+print(dt)
+
+dt = np.dtype([('age', np.int8)])
+a = np.array([(10,), (20,), (30,)], dtype=dt)
+# print(a)
+print(a['age'])
+
+
+# student = np.dtype([('name', 'S20'), ('age','i1'),('marks','f4')])
+student = np.dtype([('name','S20'), ('age', 'i1'), ('marks', 'f4')]) 
+print(student)
