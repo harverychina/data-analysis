c312ac9e7c1a8c82fdf732ab77a4ccdb0f774338
harverychina <harverychina@126.com>
2022-01-13 10:18:56 +0800

    更新Numpy数据类型

--- Numpy数据类型.py
+++ Numpy数据类型.py
@@ -23,3 +23,8 @@ print(a['age'])
 # student = np.dtype([('name', 'S20'), ('age','i1'),('marks','f4')])
 student = np.dtype([('name','S20'), ('age', 'i1'), ('marks', 'f4')]) 
 print(student)
+
+
+student = np.dtype([('name','S20'), ('age','i1'), ('marks','f4')])
+a = np.array([('abc', 21, 50), ('xyz', 18, 75)], dtype = student)
+print(a)
